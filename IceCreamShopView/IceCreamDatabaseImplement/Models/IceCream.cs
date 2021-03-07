3f42a2ef60085b0b06b323b00f171638dbb721ed
d.yudenicheva@outlook.com <d.yudenicheva@outlook.com>
2021-03-07 23:43:29 +0400

    lab_3_Basic!

---
+++ IceCreamShopView/IceCreamDatabaseImplement/Models/IceCream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamDatabaseImplement.Models
+{
+    public class IceCream
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("IceCreamId")]
+        public virtual List<IceCreamIngredient> IceCreamIngredients { get; set; }
+
+        [ForeignKey("IceCreamId")]
+        public virtual List<Order> Order { get; set; }
+
+        [Required]
+        public string IceCreamName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
